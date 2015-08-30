f2ba21cd1f3e4a0d3d962bbdc46fe819e4344c82
Leandro Toledo <leandrotoledodesouza@gmail.com>
2015-08-30 15:43:38 -0300

    Adding Arduino code

---
+++ arduino/.build/uno/NDEF/Ndef.d
@@ -0,0 +1,11 @@
+.build/uno/NDEF/Ndef.d .build/uno/NDEF/Ndef.o: lib/NDEF/Ndef.cpp lib/NDEF/Ndef.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
---
+++ arduino/.build/uno/NDEF/dependencies.d
@@ -0,0 +1,94 @@
+.build/uno/NDEF/NdefRecord.d .build/uno/NDEF/NdefRecord.o: lib/NDEF/NdefRecord.cpp lib/NDEF/NdefRecord.h \
+ lib/NDEF/Due.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ lib/NDEF/Ndef.h
+.build/uno/NDEF/NfcTag.d .build/uno/NDEF/NfcTag.o: lib/NDEF/NfcTag.cpp lib/NDEF/NfcTag.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ lib/NDEF/NdefMessage.h lib/NDEF/Ndef.h lib/NDEF/NdefRecord.h \
+ lib/NDEF/Due.h
+.build/uno/NDEF/NfcAdapter.d .build/uno/NDEF/NfcAdapter.o: lib/NDEF/NfcAdapter.cpp lib/NDEF/NfcAdapter.h \
+ lib/PN532/PN532Interface.h lib/PN532/PN532.h lib/PN532/PN532Interface.h \
+ lib/NDEF/NfcTag.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ lib/NDEF/NdefMessage.h lib/NDEF/Ndef.h lib/NDEF/NdefRecord.h \
+ lib/NDEF/Due.h lib/NDEF/MifareClassic.h lib/NDEF/MifareUltralight.h
+.build/uno/NDEF/Ndef.d .build/uno/NDEF/Ndef.o: lib/NDEF/Ndef.cpp lib/NDEF/Ndef.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
+.build/uno/NDEF/MifareUltralight.d .build/uno/NDEF/MifareUltralight.o: lib/NDEF/MifareUltralight.cpp \
+ lib/NDEF/MifareUltralight.h lib/PN532/PN532.h lib/PN532/PN532Interface.h \
+ lib/NDEF/NfcTag.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ lib/NDEF/NdefMessage.h lib/NDEF/Ndef.h lib/NDEF/NdefRecord.h \
+ lib/NDEF/Due.h
+.build/uno/NDEF/MifareClassic.d .build/uno/NDEF/MifareClassic.o: lib/NDEF/MifareClassic.cpp lib/NDEF/MifareClassic.h \
+ lib/NDEF/Due.h lib/PN532/PN532.h lib/PN532/PN532Interface.h \
+ lib/NDEF/Ndef.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ lib/NDEF/NfcTag.h lib/NDEF/NdefMessage.h lib/NDEF/NdefRecord.h
+.build/uno/NDEF/NdefMessage.d .build/uno/NDEF/NdefMessage.o: lib/NDEF/NdefMessage.cpp lib/NDEF/NdefMessage.h \
+ lib/NDEF/Ndef.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ lib/NDEF/NdefRecord.h lib/NDEF/Due.h
