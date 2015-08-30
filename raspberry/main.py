1b834b4e28367299a359bf9b2bbc386a7380377c
Leandro Toledo <leandrotoledodesouza@gmail.com>
2015-08-30 17:19:20 -0300

    main -> raspberry

--- raspberry/main.py
+++ raspberry/main.py
@@ -4,10 +4,11 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     line = ser.readline().strip()
-    nfc_content = open('nfc','r')
+    nfc_content = open('../data/tags.csv','r')
     match = re.search(line, nfc_content.read(), re.M)
     nfc_content.close()
     if len(line) == 20 and match == None:
-        nfc = open('nfc','a')
+        nfc = open('../data/tags.csv','a')
+	print(line)
         nfc.write(line+'\n')
         nfc.close()
