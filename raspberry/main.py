b14169c1217594b1bc62ed2c7336f720696c5b1a
PedroGabriel <pedrogabriel.delarosa@gmail.com>
2015-08-30 17:43:42 -0300

    Send checking to bot

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,5 +1,7 @@
 import serial
 import re
+from bot import ToledosPalaceBot
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
@@ -9,6 +11,10 @@ while True:
     nfc_content.close()
     if len(line) == 20 and match == None:
         nfc = open('../data/tags.csv','a')
-	print(line)
+        print(line)
         nfc.write(line+'\n')
         nfc.close()
+    else:
+    	find = line.find(',')
+    	if find:
+    		ToledosPalaceBot.checkIn(line[find:])
