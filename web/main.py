4050e75c0b0eea9b393d7452abc9bbc92c1857b1
Leandro Toledo <leandrotoledodesouza@gmail.com>
2015-08-30 18:58:58 -0300

    Final push

--- web/main.py
+++ web/main.py
@@ -4,9 +4,37 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
+       with open('../data/tags.csv', 'rb') as file:
+          reader = csv.reader(file)
+          tags = [row for row in reader]
+
+       return render_template('index.html', tags=tags)
+    if request.method == 'POST':
+       new = []
+       with open('../data/tags.csv', 'rb') as file:
+          reader = csv.reader(file)
+          for row in reader:
+             if row[0] == request.form['id-nfc']:
+                try:
+                   row[1] = request.form['name-nfc']
+                except IndexError:
+                   row.append(request.form['name-nfc'])
+
+             new.append(row)
+
+       print new
+   
+       with open('../data/tags.csv', 'wb') as file:
+          for row in new:
+             if len(row) == 2:
+                file.write('%s,%s\n' % (row[0], row[1]))
+             else:
+                file.write('%s\n' % (row[0],))
+
+
        with open('../data/tags.csv', 'rb') as file:
           reader = csv.reader(file)
           tags = [row for row in reader]
