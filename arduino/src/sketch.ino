a67369e0cc23f6b1b9be41ca02f9990900209d56
Leandro Toledo <leandrotoledodesouza@gmail.com>
2015-08-30 15:59:38 -0300

    Fix arduino serial nfc reader

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -3,22 +3,20 @@
 #include "PN532.h"
 #include "NfcAdapter.h"
  
-PN532_SPI interface(SPI, 10); // create a PN532 SPI interface with the SPI CS terminal located at digital pin 10
-NfcAdapter nfc = NfcAdapter(interface); // create an NFC adapter object
+PN532_SPI interface(SPI, 10);
+NfcAdapter nfc = NfcAdapter(interface);
  
 void setup(void) {
-    Serial.begin(115200); // begin serial communication
-    Serial.println("NDEF Reader");
-    nfc.begin(); // begin NFC communication
+    Serial.begin(9600);
+    nfc.begin();
 }
  
 void loop(void) {
 
-    Serial.println("\nScan an NFC tag\n");
-    if (nfc.tagPresent()) // Do an NFC scan to see if an NFC tag is present
+    if (nfc.tagPresent())
     {
-        NfcTag tag = nfc.read(); // read the NFC tag into an object, nfc.read() returns an NfcTag object.
-        tag.print(); // prints the NFC tags type, UID, and NDEF message (if available)
+        NfcTag tag = nfc.read();
+        Serial.println(tag.getUidString());
     }
-    delay(500); // wait half a second (500ms) before scanning again (you may increment or decrement the wait time)
+    delay(500);
 }
