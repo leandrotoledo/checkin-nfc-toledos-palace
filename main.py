59ee2af7deab04783019e141af150d0a944566cc
PedroGabriel <pedrogabriel.delarosa@gmail.com>
2015-08-30 17:15:13 -0300

    Second commit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import serial
+import re
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
 while True:
-    print ser.readline()
+    line = ser.readline().strip()
+    nfc_content = open('nfc','r')
+    match = re.search(line, nfc_content.read(), re.M)
+    nfc_content.close()
+    if len(line) == 20 and match == None:
+        nfc = open('nfc','a')
+        nfc.write(line+'\n')
+        nfc.close()
